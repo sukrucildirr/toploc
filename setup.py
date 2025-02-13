a6277326049a1a3679a3f85cf10d0436322d3c92
Jackmin801 <ongjackm@gmail.com>
2025-02-13 23:12:07 +0000

    use prebuilt c

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
-from setuptools import setup, Extension
+from setuptools import setup, find_packages
 import os
+from torch.utils.cpp_extension import BuildExtension, CppExtension
 
-cpp_extension = Extension(
-    'toploc.C.ndd',
-    sources=[
-        os.path.join('toploc', 'C', 'csrc', 'ndd.cpp'),
-        os.path.join('toploc', 'C', 'csrc', 'utils.cpp')
-    ],
-    include_dirs=[os.path.join('toploc', 'C', 'csrc')],
-    language='c++'
+CSRC_DIR = os.path.join('toploc', 'C', 'csrc')
+
+extensions = [
+    CppExtension(
+        name='toploc.C.csrc.ndd',
+        sources=[os.path.join(CSRC_DIR, 'ndd.cpp')],
+        extra_compile_args=['-O3'],
+        extra_link_args=['-Wl,--no-as-needed', '-lm'],
+    ),
+    CppExtension(
+        name='toploc.C.csrc.utils',
+        sources=[os.path.join(CSRC_DIR, 'utils.cpp')],
+        extra_compile_args=['-O3'],
+        extra_link_args=['-Wl,--no-as-needed', '-lm'],
+    ),
+]
+
+setup(
+    name='toploc',
+    ext_modules=extensions,
+    #packages=['toploc', 'toploc.C.csrc'],
+    packages=find_packages(),
+    package_data={
+        'toploc.C.csrc': ['*.pyi'],  # Include .pyi files
+    },
+    cmdclass={'build_ext': BuildExtension},
 )
-
-setup()
-#setup(
-#    ext_modules=[cpp_extension],
-#)
