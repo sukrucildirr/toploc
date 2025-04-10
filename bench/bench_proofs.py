fb7ae3dc6b499302dcf8bbd9905518fa250f285c
Jackmin801 <56836461+Jackmin801@users.noreply.github.com>
2025-04-09 23:12:14 -0700

    [fix] dont use triton because mac wont have it (#29)
    * dont use triton because mac wont have it
    
    * add setuptools which I think is needed by torch

--- bench/bench_proofs.py
+++ bench/bench_proofs.py
@@ -2,13 +2,13 @@ from toploc import verify_proofs_base64, verify_proofs_bytes, verify_proofs, Pro
 from toploc.poly import build_proofs_base64, build_proofs, build_proofs_bytes
 import torch
 import multiprocessing as mp
-from triton.testing import do_bench
+from torch.utils.benchmark import Timer
 
 DECODE_BATCHING_SIZE = 32
 TOPK = 128
 SKIP_PREFILL = True
 BENCH_ITERATIONS = 5
-PRINT_RESULTS = True
+PRINT_RESULTS = False
 SHAPE = (1000, 5120)
 
 
@@ -24,16 +24,21 @@ def get_deterministic_activations(
 def subproc_pure(proofs: list[ProofPoly]):
     activations = get_deterministic_activations(SHAPE, 42, "cpu")
 
-    time_ms = do_bench(
-        lambda: verify_proofs(
-            activations,
-            proofs,
-            decode_batching_size=DECODE_BATCHING_SIZE,
-            topk=TOPK,
-            skip_prefill=SKIP_PREFILL,
-        ),
-        rep=BENCH_ITERATIONS,
-        warmup=2,
+    time_ms = (
+        Timer(
+            stmt="verify_proofs(activations, proofs, decode_batching_size=DECODE_BATCHING_SIZE, topk=TOPK, skip_prefill=SKIP_PREFILL)",
+            globals={
+                "verify_proofs": verify_proofs,
+                "activations": activations,
+                "proofs": proofs,
+                "DECODE_BATCHING_SIZE": DECODE_BATCHING_SIZE,
+                "TOPK": TOPK,
+                "SKIP_PREFILL": SKIP_PREFILL,
+            },
+        )
+        .timeit(BENCH_ITERATIONS)
+        .mean
+        * 1000
     )
     print(f"Pure Time taken: {time_ms:.2f} ms")
 
@@ -44,6 +49,10 @@ def subproc_pure(proofs: list[ProofPoly]):
         topk=TOPK,
         skip_prefill=SKIP_PREFILL,
     )
+    for result in results:
+        assert result.exp_mismatches == 0
+        assert result.mant_err_mean == 0
+        assert result.mant_err_median == 0
     if PRINT_RESULTS:
         print("Results:")
         print(*results, sep="\n")
@@ -52,16 +61,21 @@ def subproc_pure(proofs: list[ProofPoly]):
 def subproc_bytes(proofs: list[bytes]):
     activations = get_deterministic_activations(SHAPE, 42, "cpu")
 
-    time_ms = do_bench(
-        lambda: verify_proofs_bytes(
-            activations,
-            proofs,
-            decode_batching_size=DECODE_BATCHING_SIZE,
-            topk=TOPK,
-            skip_prefill=SKIP_PREFILL,
-        ),
-        rep=BENCH_ITERATIONS,
-        warmup=2,
+    time_ms = (
+        Timer(
+            stmt="verify_proofs_bytes(activations, proofs, decode_batching_size=DECODE_BATCHING_SIZE, topk=TOPK, skip_prefill=SKIP_PREFILL)",
+            globals={
+                "verify_proofs_bytes": verify_proofs_bytes,
+                "activations": activations,
+                "proofs": proofs,
+                "DECODE_BATCHING_SIZE": DECODE_BATCHING_SIZE,
+                "TOPK": TOPK,
+                "SKIP_PREFILL": SKIP_PREFILL,
+            },
+        )
+        .timeit(BENCH_ITERATIONS)
+        .mean
+        * 1000
     )
     print(f"Bytes Time taken: {time_ms:.2f} ms")
 
@@ -72,6 +86,10 @@ def subproc_bytes(proofs: list[bytes]):
         topk=TOPK,
         skip_prefill=SKIP_PREFILL,
     )
+    for result in results:
+        assert result.exp_mismatches == 0
+        assert result.mant_err_mean == 0
+        assert result.mant_err_median == 0
     if PRINT_RESULTS:
         print("Results:")
         print(*results, sep="\n")
@@ -80,16 +98,21 @@ def subproc_bytes(proofs: list[bytes]):
 def subproc_base64(proofs: list[str]):
     activations = get_deterministic_activations(SHAPE, 42, "cpu")
 
-    time_ms = do_bench(
-        lambda: verify_proofs_base64(
-            activations,
-            proofs,
-            decode_batching_size=DECODE_BATCHING_SIZE,
-            topk=TOPK,
-            skip_prefill=SKIP_PREFILL,
-        ),
-        rep=BENCH_ITERATIONS,
-        warmup=2,
+    time_ms = (
+        Timer(
+            stmt="verify_proofs_base64(activations, proofs, decode_batching_size=DECODE_BATCHING_SIZE, topk=TOPK, skip_prefill=SKIP_PREFILL)",
+            globals={
+                "verify_proofs_base64": verify_proofs_base64,
+                "activations": activations,
+                "proofs": proofs,
+                "DECODE_BATCHING_SIZE": DECODE_BATCHING_SIZE,
+                "TOPK": TOPK,
+                "SKIP_PREFILL": SKIP_PREFILL,
+            },
+        )
+        .timeit(BENCH_ITERATIONS)
+        .mean
+        * 1000
     )
     print(f"Base64 Time taken: {time_ms:.2f} ms")
 
@@ -100,6 +123,10 @@ def subproc_base64(proofs: list[str]):
         topk=TOPK,
         skip_prefill=SKIP_PREFILL,
     )
+    for result in results:
+        assert result.exp_mismatches == 0
+        assert result.mant_err_mean == 0
+        assert result.mant_err_median == 0
     if PRINT_RESULTS:
         print("Results:")
         print(*results, sep="\n")
